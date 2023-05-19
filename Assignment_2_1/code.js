0a546bba6fdfbdf7ee70252304b7fae9f50b81fc
vandalfar <micande1@uat.edu>
2023-05-19 15:13:46 -0500

    update 2

--- Assignment_2_1/code.js
+++ Assignment_2_1/code.js
@@ -1,14 +1,9 @@
 //Created by Michael Anderson
-/* Initialize global variables for character and opponent
-var charArmorClass = 17;
-var charHitPoints = 12;
-var charInit = charInitRoll - 1;
-var charProf = 2;
-//gnoll opponent variables
-var gnollArmorClass = 15;
-var gnollHitPoints = 22;
-var gnollInit = gnollInitRoll;
-var gnollProf = 4; */
+function gameOver()
+{
+    alert(`You have been knocked unconcious.
+Game Over.`)
+}
 function runAlert()
 {
     alert(`You run ahead to alert the party to the presence of the gnoll in the
@@ -21,11 +16,35 @@ Game Over.`)
 //character variables
 var charArmorClass = 17;
 var charHitPoints = 12;
-var charInit = Math.ceil(Math.random() *20 -1);
-var charAtk = Math.ceil(Math.random() *6+2);
+//used to calculate attack rolls
+var charAtk = Math.ceil(Math.random() *20+2);
+//used to calculate damage rolls
+var charDmg = Math.ceil(Math.random() *6+3);
 //gnoll opponent variables
 var gnollArmorClass = 15;
-
+var gnollHitPoints = 22;
+var gnollAtk = Math.ceil(Math.random() *20+2);
+var gnollDmg = Math.ceil(Math.random() *4+2);
 function playGame(){
-    document.getElementById("storyPrompt").innerHTML = "Your Initiative is " + charInit 
+    document.getElementById("storyPrompt").innerHTML = `The gnoll, a feral, hyena-headed humanoid notices you as you approach. It seems as though it has been caught off guard.`
+    if(charHitPoints <= 0){
+        gameOver()
+    }
+    else if(gnollHitPoints <= 0) {
+        document.getElementById("gameBoard").innerHTML = `You knocked the gnoll unconcious and successfully alerted your party back at the cave. Your adventure continues elsewhere.`
+    }
+    else if(charAtk >= gnollArmorClass){
+        gnollHitPoints = gnollHitPoints-charDmg
+        document.getElementById("gameBoard").innerHTML = `You hit the gnoll for ` + charDmg + ` points, knocking its total health down to ` + gnollHitPoints
+    }
+    else if(charAtk < gnollArmorClass){
+        document.getElementById("gameBoard").innerHTML = `You missed!`
+    }
+    else if(gnollAtk >= charArmorClass){
+        charHitPoints = charHitPoints-gnollDmg
+        document.getElementById('dmPrompt').innerHTML = `<span class="dmVoice">The gnoll hits you for ` + gnollDmg + ` points, knocking your health down to ` + charHitPoints + `</span>`
+    }
+    else if(gnollAtk < charArmorClass){
+        document.getElementById('dmPrompt').innerHTML = `<span class="dmVoice">The gnoll missed its attack! You take no damage this round.</span>`
+    }
 }

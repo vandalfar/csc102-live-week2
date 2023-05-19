32133713e15d29f86016d97e44eaca2563bf1500
vandalfar <micande1@uat.edu>
2023-05-19 15:29:13 -0500

    update 3

--- Assignment_2_1/code.js
+++ Assignment_2_1/code.js
@@ -16,24 +16,19 @@ Game Over.`)
 //character variables
 var charArmorClass = 17;
 var charHitPoints = 12;
-//used to calculate attack rolls
-var charAtk = Math.ceil(Math.random() *20+2);
-//used to calculate damage rolls
-var charDmg = Math.ceil(Math.random() *6+3);
 //gnoll opponent variables
 var gnollArmorClass = 15;
 var gnollHitPoints = 22;
-var gnollAtk = Math.ceil(Math.random() *20+2);
-var gnollDmg = Math.ceil(Math.random() *4+2);
 function playGame(){
-    document.getElementById("storyPrompt").innerHTML = `The gnoll, a feral, hyena-headed humanoid notices you as you approach. It seems as though it has been caught off guard.`
-    if(charHitPoints <= 0){
-        gameOver()
-    }
-    else if(gnollHitPoints <= 0) {
-        document.getElementById("gameBoard").innerHTML = `You knocked the gnoll unconcious and successfully alerted your party back at the cave. Your adventure continues elsewhere.`
-    }
-    else if(charAtk >= gnollArmorClass){
+    //used to calculate attack rolls
+    var charAtk = Math.ceil(Math.random() *20+2);
+    //used to calculate damage rolls
+    var charDmg = Math.ceil(Math.random() *6+3);
+    //gnoll opponent variables
+    var gnollAtk = Math.ceil(Math.random() *20+2);
+    var gnollDmg = Math.ceil(Math.random() *4+2);
+    document.getElementById("storyBoard").innerHTML = `The gnoll, a feral, hyena-headed humanoid notices you as you approach. It seems as though it has been caught off guard.`
+    if(charAtk >= gnollArmorClass){
         gnollHitPoints = gnollHitPoints-charDmg
         document.getElementById("gameBoard").innerHTML = `You hit the gnoll for ` + charDmg + ` points, knocking its total health down to ` + gnollHitPoints
     }
@@ -46,5 +41,14 @@ function playGame(){
     }
     else if(gnollAtk < charArmorClass){
         document.getElementById('dmPrompt').innerHTML = `<span class="dmVoice">The gnoll missed its attack! You take no damage this round.</span>`
+    }
+    else if(gnollHitPoints <= 0){
+        document.getElementById("gameBoard").innerHTML = `You knocked the gnoll unconcious and successfully alerted your party back at the cave. Your adventure continues elsewhere.`
+    }
+    else if(charHitPoints <= 0){
+        gameOver()
+    }
+    else{
+        document.getElementById("dmPrompt").innerHTML = `The Game Goes On`
     }
 }
